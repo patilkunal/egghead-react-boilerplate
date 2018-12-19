8372be3c0a39ae5f6e5cca59a3302a55e811b35b
Kunal Patil <Kunal.Patil@cox.com>
2018-12-19 17:25:24 -0500

    Add webpack server module to serve the file over http

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,5 +2,8 @@ const merge = require('webpack-merge')
 const baseconfig = require('./webpack.config.base')
 
 module.exports = merge(baseconfig, {
-    mode: 'development'
+    mode: 'development',
+    devServer: {
+        port: 9000
+    }
 })
