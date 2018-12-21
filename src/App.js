f6d7f44d1245cba4323b011d4d0135904a122f4d
Kunal Patil <Kunal.Patil@cox.com>
2018-12-20 19:35:49 -0500

    Lazy loading of React JS Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+const Warning = React.lazy(() => import('./Warning'))
 class App extends React.Component {
     state = {
         count: 0
@@ -10,6 +11,11 @@ class App extends React.Component {
         <h2>Count: {this.state.count}</h2>
         <button onClick={() => this.setState(state => ({count: state.count+1}))}>Incr</button>
         <button onClick={() => this.setState(state => ({count: state.count-1}))}>Decr</button>
+        {this.state.count > 10 ?
+            <React.Suspense fallback={null}>
+                <Warning />
+            </React.Suspense>
+        : null}
         </div>)
     }
 }
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -15,7 +15,10 @@ module.exports = {
                 exclude: /node_modules/,
                 options: {
                     presets: ['@babel/preset-env', '@babel/preset-react'],
-                    plugins: ['@babel/plugin-proposal-class-properties']
+                    plugins: [
+                        '@babel/plugin-proposal-class-properties',
+                        '@babel/plugin-syntax-dynamic-import'
+                    ]
                 }
             },
             {
